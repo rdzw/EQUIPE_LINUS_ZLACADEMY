f6e7807209091b391d4d73c926a660f467b0e94c
Jade Santos <santmjad@gmail.com>
2024-08-20 15:59:20 -0400

    adicionando filtros e abrindo perfil

--- BOT-LEADS-LINKEDIN/bot.py
+++ BOT-LEADS-LINKEDIN/bot.py
@@ -2,7 +2,9 @@ from botcity.web import WebBot, By
 from botcity.maestro import *
 from dotenv import load_dotenv
 from time import sleep
+
 import os
+import pandas as pd
 
 # Carrega as variáveis de ambiente
 load_dotenv()
@@ -17,6 +19,10 @@ def main():
 
     print(f"Task ID is: {execution.task_id}")
     print(f"Task Parameters are: {execution.parameters}")
+
+    #Ler o arquivo Excel
+    file_path = 'Dados-leads.xlsx'
+    #df = pd.read_excel(file_path, engine='openpyxl')
 
     # Configurar e iniciar o navegador
     bot = WebBot()
@@ -64,6 +70,29 @@ def main():
         except Exception as e:
             print("Caixa de pesquisa não encontrada, tentando novamente...")
             sleep(1)
+
+    #Adicionar filtro
+    while True:
+        try:
+            bot.find_element("#search-reusables__filters-bar > ul > li:nth-child(4) > button", By.CSS_SELECTOR).click()
+            break
+        except Exception as e:
+            print("Filtro não encontrado, tentando novamente...")
+            sleep(1)
+    
+    link_perfil = '/html/body/div[5]/div[3]/div[2]/div/div[1]/main/div/div/div[2]/div/ul/li[{}]/div/div/div/div[2]/div[1]/div[1]/div/span[1]/span/a/span/span[1]'
+    max_result = 5
+    sleep(5)
+
+    for i in range(1, max_result+1):
+        try:
+            sleep(2)
+            bot.find_element(link_perfil.format(i), By.XPATH).click()
+            sleep(5)
+            bot.back()
+        except Exception as e:
+            print(f"Erro ao clicar no resultado {i}: {e}") 
+
 
     bot.wait(5000)
     bot.stop_browser()
