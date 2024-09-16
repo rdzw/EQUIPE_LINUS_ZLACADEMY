117fac5f0dc26b8ee58d67a85b7c8d483518e21b
Jade Santos <santmjad@gmail.com>
2024-09-16 15:51:46 -0400

    extract pdf data

--- Desafios LG/Desafio 2 - Extração de Informações de um PDF Fictício e Envio por Email/bot_PDF_Extractor_e_Envio/bot.py
+++ Desafios LG/Desafio 2 - Extração de Informações de um PDF Fictício e Envio por Email/bot_PDF_Extractor_e_Envio/bot.py
@@ -1,8 +1,24 @@
-from botcity.core import DesktopBot
-
-from botcity.maestro import *
+from botcity.maestro import * 
+import pandas as pd
+import PyPDF2
 
 BotMaestroSDK.RAISE_NOT_CONNECTED = False
+
+def extract_pdf_data(pdf_path):
+    with open(pdf_path,'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        num_pages = len(reader.pages)
+        text = ''
+
+        for page_num in range(num_pages):
+            page = reader.pages[page_num]
+            page_text = page.extract_text()
+            if page_text:
+                text += page_text
+            else:
+                print(f"Página {page_num + 1} está vazia ou não contém texto.")
+            
+    return text
 
 def main():
     maestro = BotMaestroSDK.from_sys_args()
@@ -10,6 +26,12 @@ def main():
 
     print(f"Task ID is: {execution.task_id}")
     print(f"Task Parameters are: {execution.parameters}")
+
+    pdf_path = 'PDF\Credenciamento_003_Edital_009_2023_-_Circulacao.pdf'
+
+    pdf_text = extract_pdf_data(pdf_path)
+
+    print(pdf_text)
 
 def not_found(label):
     print(f"Element not found: {label}")
