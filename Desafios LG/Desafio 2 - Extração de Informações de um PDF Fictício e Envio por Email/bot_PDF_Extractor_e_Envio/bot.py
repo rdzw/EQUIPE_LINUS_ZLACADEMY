d7b74fc3cfe28516c963805aaba7a15e3fb64036
Jade Santos <santmjad@gmail.com>
2024-09-16 17:21:20 -0400

    organização do código

--- Desafios LG/Desafio 2 - Extração de Informações de um PDF Fictício e Envio por Email/bot_PDF_Extractor_e_Envio/bot.py
+++ Desafios LG/Desafio 2 - Extração de Informações de um PDF Fictício e Envio por Email/bot_PDF_Extractor_e_Envio/bot.py
@@ -6,68 +6,62 @@ import PyPDF2
 import re
 import os
 
+# Carrega as variáveis de ambiente (email e senha)
 load_dotenv()
 EMAIL = os.getenv('EMAIL')
 PASSWORD = os.getenv('PASSWORD')
 
 BotMaestroSDK.RAISE_NOT_CONNECTED = False
 
+#Extrai o texto de um arquivo PDF.
 def extract_pdf_data(pdf_path):
-    with open(pdf_path,'rb') as file:
+    with open(pdf_path, 'rb') as file:
         reader = PyPDF2.PdfReader(file)
-        text = ""
-        for page in reader.pages:
-            text += page.extract_text() or ""         
-    return text
+        return "".join(page.extract_text() or "" for page in reader.pages)
 
+#Extrai uma tabela de dados formatada a partir do texto usando regex.
 def extract_table(text):
-
     pattern = re.compile(r'(\d+)\s+([\w\s]+)\s+([\w\s,]+)\s+(\d+h)\s+R\$ (\d+\.\d{3},\d{2})')
-    matches = pattern.findall(text)
-    data = [list(match) for match in matches]
+    return [list(match) for match in pattern.findall(text)]
 
-    return data
-
+#Salva os dados extraídos em um arquivo Excel.
 def save_to_excel(data, excel_path):
     columns = ['Item', 'Segmento Artístico', 'Tipo', 'Duração', 'Valor']
-    df = pd.DataFrame(data, columns=columns)
-    df.to_excel(excel_path, index=False)
+    pd.DataFrame(data, columns=columns).to_excel(excel_path, index=False)
 
+#Configura e envia um email com o arquivo Excel em anexo.
 def send_email(excel_path, email):
     email.configure_smtp("imap.gmail.com", 587)
     email.login(EMAIL, PASSWORD)
 
-    # Definindo os atributos que comporão a mensagem
     para = ["<menezesandreina18@gmail.com>", "<rodrigoword2@gmail.com>"]
     assunto = "Relatório de Extração"
-    corpo_email = "<h1>Olá!</h1> Segue em anexo excel contendo dados extraídos do pdf!"
-    arquivos = [excel_path]
-
-    email.send_message(assunto, corpo_email, para, attachments=arquivos, use_html=True)
+    corpo_email = "<h1>Olá!</h1> Segue em anexo excel contendo dados extraídos do PDF!"
+    
+    # Envia o email com o arquivo em anexo
+    email.send_message(assunto, corpo_email, para, attachments=[excel_path], use_html=True)
+    print('Email enviado com sucesso!!')
 
 def main():
     maestro = BotMaestroSDK.from_sys_args()
     execution = maestro.get_execution()
 
-    print(f"Task ID is: {execution.task_id}")
-    print(f"Task Parameters are: {execution.parameters}")
+    print(f"Task ID: {execution.task_id}")
+    print(f"Task Parameters: {execution.parameters}")
 
     email = BotEmailPlugin()
 
-    pdf_path = 'PDF\Credenciamento_003_Edital_009_2023_-_Circulacao.pdf'
+    pdf_path = 'PDF/Credenciamento_003_Edital_009_2023_-_Circulacao.pdf'
     excel_path = "tabela_extraida.xlsx"
 
     pdf_text = extract_pdf_data(pdf_path)
-
     table_data = extract_table(pdf_text)
-
-    excel_path = "tabela_extraida.xlsx"
+    
     save_to_excel(table_data, excel_path)
-
     send_email(excel_path, email)
 
 def not_found(label):
-    print(f"Element not found: {label}")
+    print(f"Elemento não encontrado: {label}")
 
 if __name__ == '__main__':
     main()
