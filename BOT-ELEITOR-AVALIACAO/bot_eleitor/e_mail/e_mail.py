a7e65dc298dc282baadfa6efe25839ec1bf83ef5
Jade Santos <santmjad@gmail.com>
2024-09-30 17:00:58 -0400

    [FEAT] - envio de email

--- BOT-ELEITOR-AVALIACAO/bot_eleitor/e_mail/e_mail.py
+++ BOT-ELEITOR-AVALIACAO/bot_eleitor/e_mail/e_mail.py
@@ -4,9 +4,18 @@ import os
 
 load_dotenv()
 EMAIL = os.getenv('EMAIL')
-PASSWORD = os.getenv('PASSWORD')
+SENHA = os.getenv('PASSWORD')
 
-def enviar_email(destinatario, assunto, conteudo):
+def enviar_email(destinatario, assunto, conteudo, arquivo_anexo):
     email = BotEmailPlugin()
     email.configure_imap("imap.gmail.com", 993)
-    email.configure_smtp(host_address="smtp.gmail.com",port=587)
+    email.configure_smtp("smtp.gmail.com", 587)
+    email.login(email=EMAIL, password=SENHA)
+
+    to = [destinatario]
+    subject = assunto
+    body = conteudo
+    files = [arquivo_anexo]
+
+    email.send_message(subject, body, to, attachments=files, use_html=True)
+    email.disconnect()
