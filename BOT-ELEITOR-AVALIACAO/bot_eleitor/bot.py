7297863d4919b6c8de6ed8eaf1154c4160e01d60
Jade Santos <santmjad@gmail.com>
2024-10-01 12:13:28 -0400

    [FEAT] - acessando dados eleitorais

--- BOT-ELEITOR-AVALIACAO/bot_eleitor/bot.py
+++ BOT-ELEITOR-AVALIACAO/bot_eleitor/bot.py
@@ -2,6 +2,8 @@ from botcity.web import WebBot, Browser, By
 from botcity.maestro import *
 from webdriver_manager.chrome import ChromeDriverManager
 from botcity.plugins.http import BotHttpPlugin
+from datetime import datetime
+
 import requests
 
 import e_mail.e_mail as e_mail
@@ -16,15 +18,54 @@ def api_lista_usuarios():
 def acessar_site(bot):
     bot.browse('https://www.tse.jus.br/servicos-eleitorais/autoatendimento-eleitoral#/atendimento-eleitor?id=1727788956071')
     bot.maximize_window()
+    
+    #cookie
+    while len(bot.find_elements('//*[@id="modal-lgpd"]/div/div/div[2]/button', By.XPATH))<1:
+        bot.wait(1000)
+        print('carrengado.')
+    bot.find_element('//*[@id="modal-lgpd"]/div/div/div[2]/button', By.XPATH).click()
 
-    bot.scroll_down(clicks=4)
-                    
+    #procurar opção 10 
     while len(bot.find_elements('//*[@id="content"]/app-root/div/app-atendimento-eleitor/div[1]/app-menu-option[10]/button', By.XPATH))<1:
         bot.wait(1000)
         print('carrengado.')
 
-    bot.find_element('//*[@id="content"]/app-root/div/app-atendimento-eleitor/div[1]/app-menu-option[10]/button', By.XPATH).click()
-    bot.wait(1000)
+    excel_path = 'planilha\RelacaoEleitor.xlsx' 
+    sheet = 'CPF'  
+    df = planilha.ler_excel(excel_path, sheet) 
+
+    for index, row in df.iterrows():
+        cpf = row['CPF']
+        data_nascimento = row['DATA_NASCIMENTO']
+        nome = row['NOME']
+        nome_mae = row['NOME_MAE']
+        cep = row['CEP']
+        nro_endereco = row['NRO_ENDERECO']
+
+        data_convertida = data_nascimento.strftime('%d/%m/%Y')
+
+        bot.find_element('//*[@id="content"]/app-root/div/app-atendimento-eleitor/div[1]/app-menu-option[10]/button', By.XPATH).click()
+
+        while len(bot.find_elements('/html/body/main/div/div/div[3]/div/div/app-root/app-modal-auth/div/div/div/div/div[2]/div[2]/form/div[1]/div[1]/input', By.XPATH))<1:
+            bot.wait(1000)
+            print('carrengado.')
+
+        bot.find_element('/html/body/main/div/div/div[3]/div/div/app-root/app-modal-auth/div/div/div/div/div[2]/div[2]/form/div[1]/div[1]/input', By.XPATH).click()
+        bot.paste(str(cpf))
+        bot.wait(100)
+
+        bot.find_element('/html/body/main/div/div/div[3]/div/div/app-root/app-modal-auth/div/div/div/div/div[2]/div[2]/form/div[1]/div[2]/input', By.XPATH).click()
+        bot.paste(str(data_convertida))
+        bot.wait(100)
+
+        bot.find_element('/html/body/main/div/div/div[3]/div/div/app-root/app-modal-auth/div/div/div/div/div[2]/div[2]/form/div[1]/div[3]/div/input', By.XPATH).click()
+        bot.paste(nome_mae)
+        bot.wait(100)
+
+        bot.find_element('//*[@id="modal"]/div/div/div[2]/div[2]/form/div[2]/button[2]', By.XPATH).click()
+    
+        bot.wait(100000000)
+
 
 def main():
     maestro = BotMaestroSDK.from_sys_args()
